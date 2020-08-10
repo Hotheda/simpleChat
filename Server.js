8b1c75c8680242f1b18987dc667dd9c3136bd153
Heda <hotheda@gmail.com>
2020-08-11 01:52:24 +0200

    basics working

--- Server.js
+++ Server.js
@@ -1,10 +1,15 @@
 const io = require('socket.io')(4000)
 
 io.on('connection', socket => {
-    console.log("New user")
     socket.emit("new-user", "Hej på dig du!")
 
-    socket.on('new-user', message => {
-        console.log(message)
+    socket.on('new-user', name => {
+        console.log(name)
+        socket.brodcast.emit('chat-message', name + " joined")
+    })
+
+    socket.on('chat-message', message => {
+        console.log(socket.id, message)
+        socket.broadcast.emit('chat-message', message)
     })
 })
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useReducer } from "react"
+import React, { useState, useEffect } from "react"
 import socketIOClient from "socket.io-client"
 
 export default function App(){
   const [chatData, setChatData] = useState([])
   const [socket, setSocket] = useState(null)
+  const [messageToSend, setMessageToSend] = useState()
 
   //const socket = 
 
@@ -14,27 +15,39 @@ export default function App(){
   useEffect(()=>{
     if(!socket) return;
     
-    socket.on("new-user", data =>{
+    socket.on("new-user", data => {
       var name = prompt("Hi there, whats your name?")
-      setChatData(...chatData, data)
+      setChatData([...chatData, data])
       socket.emit("new-user", name)
     })
+
+    socket.on("chat-message", data => {
+
+      setChatData(chatData => [...chatData, data])
+    })
+
   },[socket])
   
 
   const sendMessage = () => {
     if(socket)
-      socket.emit("chat-message", "Bla bla bla")
+      socket.emit("chat-message", messageToSend)
+      setMessageToSend("")
   }
 
+
+  const onMessageChange = (e) => {
+    setMessageToSend(e.target.value)
+  }
 
   return(
     <div>
       <h1>Här är min frontend</h1>
       <div>
+        <input id="chat-input" value={messageToSend} onChange={(e) => onMessageChange(e)} />
         <button onClick={sendMessage}>Send message</button>
       </div>
-      {chatData}
+      {chatData ? chatData.map( item => <p>{item}</p> ) : null}
     </div>
   )
 }
