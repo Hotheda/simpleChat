9c2788e67eb957206b02becc79bc98295b612ea9
Heda <hotheda@gmail.com>
2020-08-11 10:03:23 +0200

    User names

--- Server.js
+++ Server.js
@@ -5,13 +5,13 @@ io.on('connection', socket => {
     socket.emit("new-user", "Hej på dig du!")
 
     socket.on('new-user', name => {
-        console.log(name)
+        console.log("User joined: " + name + " - " + socket.id)
         socket.name = name
-        socket.broadcast.emit('chat-message', name + " joined")
+        socket.broadcast.emit('chat-message', name + ": joined")
     })
 
     socket.on('chat-message', message => {
         console.log(socket.id, message)
-        socket.broadcast.emit('chat-message', socket.name + " " + message)
+        socket.broadcast.emit('chat-message', socket.name + ": " + message)
     })
 })
--- src/App.js
+++ src/App.js
@@ -4,37 +4,44 @@ import socketIOClient from "socket.io-client"
 export default function App(){
   const [chatData, setChatData] = useState([])
   const [socket, setSocket] = useState(null)
+  const [chatName, setChatName] = useState("Default")
   const [messageToSend, setMessageToSend] = useState()
 
   //const socket = 
 
   useEffect(()=>{
-    setSocket(socketIOClient("http://localhost:4000"))
+    setSocket(socketIOClient("http://192.168.1.153:4000"))
   },[])
 
   useEffect(()=>{
     if(!socket) return;
     
     socket.on("new-user", data => {
-      var name = prompt("Hi there, whats your name?")
+      var name = null
+      //while(!name)
+        name = prompt("Hi there, whats your name?")
+
+      setChatName(name)
       setChatData([...chatData, data])
       socket.emit("new-user", name)
     })
 
     socket.on("chat-message", data => {
-
-      setChatData(chatData => [...chatData, data])
+      var myMessage = <div> <p>{data}</p> <hr/> </div>;
+      setChatData(chatData => [...chatData, myMessage])
     })
 
   },[socket])
   
 
-  const sendMessage = () => {
+  const sendMessage = (e) => {
+    e.preventDefault()
     if(socket)
       socket.emit("chat-message", messageToSend)
+      var myMessage = <div> <p style={{color: "red"}} >You: {messageToSend}</p> <hr/> </div>;
+      setChatData(chatData => [...chatData, myMessage])
       setMessageToSend("")
   }
-
 
   const onMessageChange = (e) => {
     setMessageToSend(e.target.value)
@@ -42,12 +49,12 @@ export default function App(){
 
   return(
     <div>
-      <h1>Här är min frontend</h1>
-      <div>
+      <h1>Welcome to the chat {chatName}</h1>
+      <form>
         <input id="chat-input" value={messageToSend} onChange={(e) => onMessageChange(e)} />
-        <button onClick={sendMessage}>Send message</button>
-      </div>
-      {chatData ? chatData.map( item => <p>{item}</p> ) : null}
+        <button onClick={(e) => sendMessage(e) }>Send message</button>
+      </form>
+      {chatData ? chatData.map( item => <div>{item}</div> ) : null}
     </div>
   )
 }
